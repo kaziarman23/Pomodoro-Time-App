fbfece7a8eceaf557b4f5d077a700075883083db
kaziarman23 <kaziarman@proton.me>
2024-04-08 13:14:52 +0600

    new buttons are added in the top

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,29 @@
+const smallEl = document.querySelector(".small");
+const middleEl = document.querySelector(".middle");
+const largEl = document.querySelector(".larg");
 const timerEl = document.querySelector(".timer");
 const startEl = document.querySelector(".Start");
 const stopEl = document.querySelector(".Stop");
 const resetEl = document.querySelector(".Reset");
 
-let timeLeft = 1500;
 let intervel;
+let timeLeft = settime();
+timeLeft = 300;
+
+function settime() {
+    smallEl.addEventListener("click", function () {
+        timeLeft = 300;
+    });
+
+    middleEl.addEventListener("click", function () {
+        timeLeft = 900;
+    });
+
+    largEl.addEventListener("click", function () {
+        timeLeft = 1500;
+    });
+}
+
 
 function updateTime() {
     let minutes = Math.ceil(timeLeft / 60);
@@ -23,20 +42,22 @@ startEl.addEventListener("click", function () {
         timeLeft--;
         updateTime();
     }, 1000);
-    startEl.style.opacity = "0.6"
-    stopEl.style.opacity = "1"
+    startEl.style.opacity = "0.6";
+    stopEl.style.opacity = "1";
 });
 
 stopEl.addEventListener("click", function () {
-    clearInterval(intervel)
-    startEl.style.opacity = "1"
-    stopEl.style.opacity = "0.6"
+    clearInterval(intervel);
+    startEl.style.opacity = "1";
+    stopEl.style.opacity = "0.6";
 });
 
 resetEl.addEventListener("click", function () {
-    clearInterval(intervel)
-    timeLeft = 1500;
-    updateTime()
-    startEl.style.opacity = "1"
-    stopEl.style.opacity = "1"
+    clearInterval(intervel);
+    timeLeft = 300;
+    updateTime();
+    startEl.style.opacity = "1";
+    stopEl.style.opacity = "1";
 });
+
+
